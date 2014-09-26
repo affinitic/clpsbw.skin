709b5220f610d02f83148415a3abd6d172ffe6dd
Alain Meurant <alain.meurant@affinitic.be>
2014-09-27 00:14:28 +0200

    Retire une ligne blanche

--- src/clpsbw/skin/browser/navigation.py
+++ src/clpsbw/skin/browser/navigation.py
@@ -3,6 +3,6 @@ from plone.app.portlets.portlets.navigation import Renderer
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class MyNavRenderer(Renderer):
-    
+
      _template = ViewPageTemplateFile('templates/navigation/navigation.pt')
      recurse = ViewPageTemplateFile('templates/navigation/navigation_recurse.pt')
